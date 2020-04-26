26abdda86c233af1cf831d36efa1657f058eace1
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-04-26 19:44:19 -0400

    more tests

--- src/components/boardsquare.js
+++ src/components/boardsquare.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 
-
 class BoardSquare extends Component {
   constructor(props)  {
     super(props);
--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import  BoardSquare from './boardSquare.js'
+import BoardSquare from './boardSquare.js'
 import Uploads from './uploadSquares.js'
 import Footer from './footer.js'
 
