46e896a7dc422517ad45e97bce0c58948b3641ee
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-04-26 21:18:03 -0400

    added some more game logic and the winner modal

--- src/components/main.js
+++ src/components/main.js
@@ -15,7 +15,8 @@ class Main extends Component  {
       breedData: [],
       showmodal: false,
       checked: [],
-      bingo: false
+      bingo: false,
+      wonGame: false
     }
   }
 
@@ -40,6 +41,19 @@ class Main extends Component  {
     })
   }
 
+  winGame = () => {
+    this.setState({
+      wonGame: true
+    })
+  }
+
+  goBack = () => {
+    console.log('banes');
+    this.setState({
+      wonGame: false
+    })
+  }
+
 
 componentDidMount() {
       this.getDogs();
@@ -50,12 +64,21 @@ render()  {
 
   return(
       <React.Fragment>
+        <div className={this.state.wonGame ? "winner" : "none"}>
+          <h1>Congratulations!</h1>
+            <h2>That's a bingo.</h2>
+            <img src="http://24.media.tumblr.com/tumblr_m9umu7wfK51ruzepko1_500.gif"></img>
+            <div className="buttons">
+              <button className="new" onClick={() => window.location.reload()}>New Game?</button>
+              <button className="new" onClick={this.goBack}>Go Back?</button>
+            </div>
+        </div>
         <Uploads
           modalState={this.state.showmodal}
           modalChange={this.modalShow}
           formInputs={this.props.formInputs}
         />
-        <div className={this.state.bingo ? "show" : "hide"}><button>BINGO?</button></div>
+        <div className={this.state.bingo ? "show" : "hide"}><button onClick={this.winGame}>BINGO?</button></div>
         <div className={this.state.showmodal ? "blurBoard" : "boardContain"}>
           {this.state.breedData.map((dogData) => (
             <Square
